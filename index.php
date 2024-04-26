401d6aa32e405cafbad5f5bd7d4a627a79149e00
Juliprogram <pelukman47155796@gmail.com>
2024-04-25 23:12:37 -0300

    nueva tienda

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <script src="java/style.js"></script>
+</head>
+<body id="inicio">
+    <div class="header-envios fondoEnvios">
+        <p class="header-envios__text">
+            <a href="">
+                ENVIOS A PARTIR DE 90,000 25% off con trasnferencia o 3 CUOTAS SIN INTERES
+            </a>
+        </p>
+
+    </div>
+</body>
+</html>
