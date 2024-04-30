8eabba544e8ad84c45ae85a6c9a6fb0f9b8f8570
JuliProgram <pelukman47155796@gmail.com>
2024-04-30 04:14:28 -0300

    ultima

--- index.php
+++ index.php
@@ -7,17 +7,119 @@
     <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/estilos.css">
     <script src="java/style.js"></script>
 </head>
 <body id="inicio">
-    <div class="header-envios fondoEnvios">
+    <div class="header-envios fondoEnvios fixed-top" id="mainNav">
         <p class="header-envios__text">
             <a href="">
                 ENVIOS A PARTIR DE 90,000 25% off con trasnferencia o 3 CUOTAS SIN INTERES
             </a>
         </p>
-
     </div>
+    <div class="navbar-container">
+    <nav class="navbar navbar-expand-lg fixed-top" id="secondNav">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+   </div>
+   <div>
+   <div class="collapse navbar-collapse" id="secondNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">INICIO</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PRODUCTOS
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            INFORMACION
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">PUNTOS DE ENCUENTRO</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">BLOG</a>
+        </li>
+        <li class="nav-item p-2">
+            <i class="px-2 fa-solid fa-user"></i>  
+        </li>
+        <li class="nav-item p-2 ">
+            <i class="px-2 fa-solid fa-heart"></i>  
+        </li>
+        <li class="nav-item p-2">
+            <i class="px-2 fa-solid fa-cart-shopping"></i>  
+        </li>
+      </ul>
+    </div>
+   </div> 
+  </div>
+  <section>
+  <div class="carrousell mt-2">
+      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/invierno photoshoppng.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+    <h1>Descuentos!</h1>
+    <p>Aprovecha y ahorra en las siguientes prendas</p>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/inviernopho2.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+    <h1>Descuentos!</h1>
+    <p>Aprovecha y ahorra en las siguientes prendas</p>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/hombre-invierno.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+    <h1>Descuentos!</h1>
+    <p>Aprovecha y ahorra en las siguientes prendas</p>
+    </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+      </div>
+  </section>
+  <section>
+    <div class="py-5 text-center">
+        <h2>OFERTAS IMPERDIBLES</h2>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">dasd</div>
+            <div class="col-3">asfas</div>
+            <div class="col-3">dasd</div>
+            <div class="col-3">asfas</div>
+        </div>
+    </div>
+    
+
+  </section>
 </body>
 </html>
