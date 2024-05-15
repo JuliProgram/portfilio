3c970647384fffd2a697faf9eb8b2637e5f8096b
JuliProgram <tu_email@example.com>
2024-05-15 16:07:58 -0300

    first commit

--- index.php
+++ index.php
@@ -1,185 +1,61 @@
+<?php 
+ $pg = "inicio";
+
+?>
+
 <!DOCTYPE html>
-<html lang="en" class="h-100">
+<html lang="es" class="h-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <title>contacto</title>
     <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
     <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/estilos.css">
-    <script src="java/style.js"></script>
 </head>
-<body id="inicio">
-    <header>
-        <?php include_once("menuNavegador.php") ?>
+<body id="inicio" class="d-flex flex-column h-100">
+    <header class="container">
+        <?php include_once("menu.php");?>
     </header>
-  <section>
-  <div class="carrousell mt-2">
-      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="images/invierno photoshoppng.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-    <h1>Descuentos!</h1>
-    <p>Aprovecha y ahorra en las siguientes prendas</p>
-    </div>
-    </div>
-    <div class="carousel-item">
-      <img src="images/inviernopho2.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-    <h1>Descuentos!</h1>
-    <p>Aprovecha y ahorra en las siguientes prendas</p>
-    </div>
-    </div>
-    <div class="carousel-item">
-      <img src="images/hombre-invierno.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-    <h1>Descuentos!</h1>
-    <p>Aprovecha y ahorra en las siguientes prendas</p>
-    </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-      </div>
-  </section>
-  <section>
-    <div class="py-5 text-center">
-        <h2>OFERTAS IMPERDIBLES</h2>
-    </div>
-    <div class="container">
+    <main class="container">
         <div class="row">
-            <div class="col-3 text-center imgProductos">
-              <a href="invierno1.php">
-              <img class="" src="images/invierno.jpg" alt="">
-                <h3 class="text-center descripcionProducto">Abrigo Invierno</h3>
-                <p class="precioProducto text-center">$45.000,00</p>
-                <p class="descripcionAdicional text-center">3 cuotas sin interes $15.000,00</p>
-              </a>
-              </div>
-            <div class="col-3 text-center imgProductos">
-              <img src="images/invierno.jpg" alt="">
-              <h3 class="text-center descripcionProducto">Abrigo Invierno</h3>
-                <p class="precioProducto text-center">$45.000,00</p>
-                <p class="descripcionAdicional text-center">3 cuotas sin interes $15.000,00</p>
-            </div>
-            <div class="col-3 text-center imgProductos">
-              <img src="images/invierno.jpg" alt="">
-              <h3 class="text-center descripcionProducto">Abrigo Invierno</h3>
-                <p class="precioProducto text-center">$45.000,00</p>
-                <p class="descripcionAdicional text-center">3 cuotas sin interes $15.000,00</p>
-            </div>
-            <div class="col-3 text-center imgProductos">
-              <img src="images/invierno.jpg" alt="">
-              <h3 class="text-center descripcionProducto">Abrigo Invierno</h3>
-                <p class="precioProducto text-center">$45.000,00</p>
-                <p class="descripcionAdicional text-center">3 cuotas sin interes $15.000,00</p>
-            </div>
+          <div class="col-12 mt-4 text-center div-cohete">
+            <a href="proyectos.html"><img src="images/cohete.svg" alt="cohete"></a>
+          </div>
         </div>
         <div class="row">
-            <div class="col-3 text-center imgProductos">
-              <img src="images/invierno.jpg" alt="">
-                <h3 class="text-center descripcionProducto">Abrigo Invierno</h3>
-                <p class="precioProducto text-center">$45.000,00</p>
-                <p class="descripcionAdicional text-center">3 cuotas sin interes $15.000,00</p>
-            </div>
-            <div class="col-3 text-center imgProductos">
-              <img src="images/invierno.jpg" alt="">
-              <h3 class="text-center descripcionProducto">Abrigo Invierno</h3>
-                <p class="precioProducto text-center">$45.000,00</p>
-                <p class="descripcionAdicional text-center">3 cuotas sin interes $15.000,00</p>
-            </div>
-            <div class="col-3 text-center imgProductos">
-              <img src="images/invierno.jpg" alt="">
-              <h3 class="text-center descripcionProducto">Abrigo Invierno</h3>
-                <p class="precioProducto text-center">$45.000,00</p>
-                <p class="descripcionAdicional text-center">3 cuotas sin interes $15.000,00</p>
-            </div>
-            <div class="col-3 text-center imgProductos">
-              <img src="images/invierno.jpg" alt="">
-              <h3 class="text-center descripcionProducto">Abrigo Invierno</h3>
-                <p class="precioProducto text-center">$45.000,00</p>
-                <p class="descripcionAdicional text-center">3 cuotas sin interes $15.000,00</p>
-            </div>
+          <div class=" col-sm-6 my-4 mt-5 text-center mb-3 offset-sm-3">
+            <p class="py-1">Bienvenid@ a mi sitio web donde encontraras mis habilidades.</p>
+          </div>
         </div>
-    </div>
-      <div class="email-barra mt-5 px-0">
-        <div class="">
-            <div class="col-12 text-center">
-            <div class=" text-center px-0">
-              <h5 class="text-center py-2">Dejanos tu mail para recibir novedades</h5>
-            <form action="" method="">
-                <label for="">
-                  <input type="text" placeholder="Tu email">
-                  <button class="btn btn-dark">Enviar</button>
-                </label>
-              </form>
-            
-            </div>         
+        <div class="row text-center">
+          <div class="col-12">
+            <a href="proyectos.php" class="btn">Conoce mis proyectos</a>
+          </div>
         </div>
-        </div>
+    </main>
+    <footer class="container mt-auto py-4">
+      <div class="row">
+         <div class="col-12 col-sm-3 pt-3">
+          <a href="https://github.com/" target="_blank">
+              <i class="fa-brands fa-github"></i></a>
+          <a href="https://ar.linkedin.com/" target="_blank">
+              <i class="fa-brands fa-linkedin-in"></i></a>
+         </div>
+         <div class="col-12 col-sm-3 pt-3 link-blanco">
+          Sponsor <a href="https://depcsuite.com/?v=d72a48a8ebd2">DePc Suite</a>
+         </div>
+         <div class="col-12 col-sm-3 pt-3 link-blanco">
+          <a href="mailto:torres_226@hotmail.com">torres_226@hotmail.com</a>
+         </div>
+         <a href="https://api.whatsapp.com/send?phone=+541126161384">
+          <i class="fa-brands fa-whatsapp px-3 pt-3 pb-4"></i>
+         </a>
       </div>
-    <footer>
-      <div class="footer_items">
-        <div class="container">
-          <div class="row">
-          <div class="col-3">
-            <h5 class="my-3">Metodos de Pago</h5>
-            <div class="footer_items">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/mercadopago.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/mastercard.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/visa.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/american-express.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/naranja.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/cabal.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/maestro.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/diners-club.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/nativa.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/argencard.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/pagofacil.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/rapipago.png" class="footer__top__icon" alt="Medio Pago">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-pago/deposito.png" class="footer__top__icon" alt="Medio Pago">
-            </div>
-          </div>
-          <div class="col-3 mx-5 text-center">
-            <div class="">
-              <h5 class="my-3">Medios de Envio</h5>
-              <div class="footer_items">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-envio/e-pick.png" class="footer__top__icon" alt="Medio Envio">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-envio/andreani.png" class="footer__top__icon" alt="Medio Envio">
-              <img src="https://dk0k1i3js6c49.cloudfront.net/iconos-envio/retiro-local.png" class="footer__top__icon" alt="Medio Envio">
-              </div>
-            </div>
-          </div>
-          <div class="col-3">
-            <div class="">
-              <h5 class="my-3">Nuestras Redes Sociales</h5>
-              <div class="footer_items">
-              <i class="fa-brands fa-facebook"></i>
-              <i class="fa-brands fa-instagram"></i>
-
-              </div>
-            </div>
-          </div>
-          <div class="col-3 p-0">
-            
-          </div>
-          </div>
-        </div>
-
-      </div>
-    </footer>
-    
-
-  </section>
+  </footer>
+</body>
+</html>
 </body>
 </html>
